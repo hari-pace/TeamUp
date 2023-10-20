e2e3393933bb9c265533329029899f896339ff14
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-20 14:47:56 +0200

    core header done, click login to see avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+export default function Avatar() {
+    return (
+        <div className="avatarMini">
+        Avatar
+        </div>
+    )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import Logo from "./Logo"
+import Avatar from "./Avatar"
+import { useState } from "react"
 
 export default function Header() {
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
-  <div>Logo</div>
+<>
+<div className="header">
+<div className="headerLeft">
+<Logo />
+</div>
+<div className="headerRight">
+  {loggedIn ? (
+    <>
+  <Avatar />
+  <div className="loginSignup"> 
+  <button onClick={() => setLoggedIn(false)}>Logout</button>
+  </div>
+  </>
+  )
+  : (
+  <div className="loginSignup"> 
+  <button onClick={() => setLoggedIn(true)}>Login</button>
+  <button>Signup</button>
+  </div>)}
+  </div>
+  </div>
+</>
   )
 }
-
