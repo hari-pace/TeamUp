12674053d0904ca64d3fe869aa0bbf1e9e7c00ce
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-22 20:01:04 +0200

    added scollable hero and ant buttons

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ParallaxProvider } from "react-scroll-parallax"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ParallaxProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ParallaxProvider>
   </React.StrictMode>
 );
