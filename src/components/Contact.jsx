00e98b4d6f94f5e5d99b2d7b80d2322db6d1a9a4
hari-pace <hari.pace23@gmail.com>
2023-11-07 15:15:36 +0100

    dark mode for about and contact us

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "./styling/contact.css";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
 
 const Contact = () => {
+  const { light, dark, isLightTheme, toggleTheme } = useContext(ThemeContext);
+
+  const themeStyles = isLightTheme ? light : dark;
   return (
     <>
-      <div className="about-us-hero-gradient-1"></div>
-      <div className="about-us-hero">
+      <div
+        className={
+          isLightTheme
+            ? "about-us-hero-gradient-1"
+            : "about-us-hero-gradient-1-dark"
+        }
+      ></div>
+      <div
+        className="about-us-hero"
+        style={{ background: themeStyles.grey, color: themeStyles.text }}
+      >
         <h1 className="about-us-heading"> Contact us</h1>
       </div>
-      <div className="about-us-hero-gradient-2"></div>
-      <div className="about-main-container">
+      <div
+        className={
+          isLightTheme
+            ? "about-us-hero-gradient-2"
+            : "about-us-hero-gradient-2-dark"
+        }
+      ></div>
+      <div
+        className="about-main-container"
+        style={{ background: themeStyles.grey, color: themeStyles.text }}
+      >
         <div className="about-heading">
           <h2 className="contact-subheading">Having issues?</h2>
           <h2>
